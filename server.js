5a452dbf31ac2b1e17ecf172d798b3a0b3ab9ad8
Antoni1830 <jemmotte08@gmail.com>
2024-11-30 21:18:02 -0500

    oldd

--- server.js
+++ server.js
@@ -1,81 +1,100 @@
 /* ******************************************
- * This server.js file is the primary file of the 
+ * This server.js file is the primary file of the
  * application. It is used to control the project.
  *******************************************/
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+const env = require('dotenv').config()
 const app = express()
-const static = require("./routes/static")
-const baseController = require("./controllers/baseController")
-const inventoryRoute = require("./routes/inventoryRoute")
-const utilities = require("./utilities/")
-const session = require("express-session")
+const session = require('express-session')
 const pool = require('./database/')
-
-
+const utilities = require('./utilities/')
+const baseController = require('./controllers/baseController')
+const static = require('./routes/static')
+const inventoryRoute = require('./routes/inventoryRoute')
+const accountRoute = require('./routes/accountRoute')
+const errorRoute = require('./routes/errorRoute')
+const bodyParser = require('body-parser')
 
 /* ***********************
  * Middleware
  * ************************/
-app.use(session({
-  store: new (require('connect-pg-simple')(session))({
-    createTableIfMissing: true,
-    pool,
-  }),
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  name: 'sessionId',
-}))
+app.use(
+    session({
+        store: new (require('connect-pg-simple')(session))({
+            createTableIfMissing: true,
+            pool,
+        }),
+        secret: process.env.SESSION_SECRET,
+        resave: true,
+        saveUninitialized: true,
+        name: 'sessionId',
+    })
+)
 
 // Express Messages Middleware
 app.use(require('connect-flash')())
-app.use(function(req, res, next){
-  res.locals.messages = require('express-messages')(req, res)
-  next()
+app.use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res)
+    next()
 })
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 /* ***********************
  * View Engine and Templates
  *************************/
-app.set("view engine", "ejs")
+app.set('view engine', 'ejs')
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at view roots
+app.set('layout', './layouts/layout') // not at views root
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
-
 // Index route
-app.get("/", utilities.handleErrors(baseController.buildHome))
-
+app.get('/', utilities.handleErrors(baseController.buildHome))
 // Inventory routes
-app.use("/inv", inventoryRoute)
+app.use('/inv', utilities.handleErrors(inventoryRoute))
+// Account routes
+app.use('/account', utilities.handleErrors(accountRoute))
+// Error routes
+app.use('/error', utilities.handleErrors(errorRoute))
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
-  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+    const heading = 'You seem to have lost a few parts?'
+    const quote = `Sorry, we couldn't find the page you're looking for :-( <br> But you could try any of the pages in the menu-bar.`
+    next({ status: 404, message: utilities.buildErrorMessage(heading, quote) })
 })
-
 /* ***********************
-* Express Error Handler
-* Place after all other middleware
-*************************/
+ * Express Error Handler
+ * Place after all other middleware
+ *************************/
 app.use(async (err, req, res, next) => {
-  let nav = await utilities.getNav()
-  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
-  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
-  res.render("errors/error", {
-    title: `Error ${err.status}` || 'Server Error',
-    message,
-    nav
-  })
+    let nav = await utilities.getNav()
+    let title
+    let message
+    console.error(`Error at: "${req.originalUrl}": ${err.status || 500}`)
+    if (err.status === 404) {
+        title = `${err.status} - PAGE NOT FOUND`
+        message = err.message
+    } else {
+        title = `500 - INTERNAL SERVER ERROR`
+        const heading = 'Oh no! There was a crash!'
+        const quote = ` Maybe try watching where you are going next time?`
+        message = utilities.buildErrorMessage(heading, quote)
+    }
+    res.render('errors/error', {
+        title,
+        message,
+        nav,
+    })
 })
-
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
@@ -84,8 +103,8 @@ const port = process.env.PORT
 const host = process.env.HOST
 
 /* ***********************
- * Log statement to confirm server operation******
+ * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+    console.log(`app listening on ${host}:${port}`)
 })
--- utilities/index.js
+++ utilities/index.js
@@ -1,4 +1,4 @@
-const invModel = require("../models/inventory-model")
+const invModel = require('../models/inventory-model')
 const Util = {}
 
 /* ************************
@@ -6,94 +6,153 @@ const Util = {}
  ************************** */
 Util.getNav = async function (req, res, next) {
     let data = await invModel.getClassifications()
-    let list = '<ul id="navList">'
-    list += '<li class="navItems"><a class="navLinks" href="/" title="Home page">Home</a></li>'
+    // console.log(data)
+    let list = '<ul>'
+    list += '<li><a href="/" title="Home page">Home</a></li>'
     data.rows.forEach((row) => {
-        list += '<li class="navItems">'
+        list += '<li>'
         list +=
-            '<a class="navLinks" href="/inv/type/' +
+            '<a href="/inv/type/' +
             row.classification_id +
             '" title="See our inventory of ' +
             row.classification_name +
             ' vehicles">' +
             row.classification_name +
-            "</a>"
-        list += "</li>"
+            '</a>'
+        list += '</li>'
     })
-    list += "</ul>"
+    list += '</ul>'
     return list
 }
 
 /* **************************************
-* Build the classification view HTML
-* ************************************ */
-Util.buildClassificationGrid = async function(data){
+ * Build the classification view HTML
+ * ************************************ */
+Util.buildClassificationGrid = async function (data) {
     let grid
-    if(data.length > 0){
+    if (data.length > 0) {
         grid = '<ul id="inv-display">'
-        data.forEach(vehicle => { 
-            grid += '<li class="invItems">'
-            grid +=  '<a class="invImgContainer" href="../../inv/detail/'+ vehicle.inv_id 
-            + '" title="View ' + vehicle.inv_make + vehicle.inv_model 
-            + ' Details"><img class="invImg" src="' + vehicle.inv_thumbnail 
-            +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
-            +' on CSE Motors"></a>'
+        data.forEach((vehicle) => {
+            grid += '<li>'
+            grid +=
+                '<a href="../../inv/detail/' +
+                vehicle.inv_id +
+                '" title="View ' +
+                vehicle.inv_make +
+                ' ' +
+                vehicle.inv_model +
+                'details"><img src="' +
+                vehicle.inv_thumbnail +
+                '" alt="Image of ' +
+                vehicle.inv_make +
+                ' ' +
+                vehicle.inv_model +
+                ' on CSE Motors" /></a>'
             grid += '<div class="namePrice">'
-            grid += '<hr class="invLine">'
+            grid += '<hr />'
             grid += '<h2>'
-            grid += '<a class="invNameLink" href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
-            + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
-            + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
+            grid +=
+                '<a href="../../inv/detail/' +
+                vehicle.inv_id +
+                '" title="View ' +
+                vehicle.inv_make +
+                ' ' +
+                vehicle.inv_model +
+                ' details">' +
+                vehicle.inv_make +
+                ' ' +
+                vehicle.inv_model +
+                '</a>'
             grid += '</h2>'
-            grid += '<p class="invPrice">$' 
-            + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</p>'
+            grid += '<span>$' + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
             grid += '</div>'
             grid += '</li>'
         })
         grid += '</ul>'
-    } else { 
+    } else {
         grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>'
     }
     return grid
 }
 
 /* **************************************
-* Build the detail view HTML
-* ************************************ */
-Util.buildDetailGrid = async function(data){
+ * Build the details view HTML
+ * ************************************ */
+Util.buildDetailsGrid = async function (data) {
     let grid
-    if(data){
-        
-        grid = '<section id="detailContainer">'
-        
-            grid += '<img class="detImg" src="' + data.inv_image 
-            +'" alt="Image of '+ data.inv_make + ' ' + data.inv_model 
-            +' on CSE Motors">'
-
-            grid += '<div class="namePriceDescription">'
-                grid += `<h2><b>${data.inv_make} ${data.inv_model} Details</b></h2>`
-
-                grid += '<div class="detailsContainer2">'
-                    grid += '<p class="detPrice"><b>Price: $' 
-                    + new Intl.NumberFormat('en-US').format(data.inv_price) + '</b></p>'
-                    grid += `<p class="detDesc"><b>Description</b>: ${data.inv_description}</p>`
-                    grid += `<p class="detColor"><b>Color</b>: ${data.inv_color}</p>`
-                    grid += `<p class="detMiles"><b>Miles</b>: ${new Intl.NumberFormat('en-US').format(data.inv_miles)}</p>`
-                grid += '</div>'
-            grid += '</div>'
-        
-        grid += '</section>'
-    } else { 
-        grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>'
+    if (data.length > 0) {
+        grid = '<article id="details-display">'
+        grid +=
+            '<img src="' +
+            data[0].inv_image +
+            '" alt="Image of ' +
+            data[0].inv_make +
+            ' ' +
+            data[0].inv_model +
+            ' on CSE Motors" />'
+        grid += '<div id="details-info">'
+        grid += '<h2>' + data[0].inv_make + ' ' + data[0].inv_model + ' ' + 'Details</h2>'
+        grid += '<p class="price">Price: $' + new Intl.NumberFormat('en-US').format(data[0].inv_price) + '</p>'
+        grid += '<p class="description"><strong>Description:</strong> ' + data[0].inv_description + '</p>'
+        grid += '<ul class="details-list">'
+        grid += '<li><span class="checkbox"></span><strong>Year:</strong> &nbsp;' + data[0].inv_year + '</li>'
+        grid += '<li><span class="checkbox"></span><strong>Make:</strong> &nbsp;' + data[0].inv_make + '</li>'
+        grid += '<li><span class="checkbox"></span><strong>Model:</strong> &nbsp;' + data[0].inv_model + '</li>'
+        grid += '<li><span class="checkbox"></span><strong>Color:</strong> &nbsp;' + data[0].inv_color + '</li>'
+        grid +=
+            '<li><span class="checkbox"></span><strong>Mileage:</strong> &nbsp;' +
+            new Intl.NumberFormat('en-US').format(data[0].inv_miles) +
+            '</li>'
+        grid +=
+            '<li><span class="checkbox"></span><strong>Classification:</strong> &nbsp;' +
+            data[0].classification_name +
+            '</li>'
+        grid += '</ul>'
+        grid += '</div>'
+        grid += '</article>'
+    } else {
+        grid += '<p class="notice">Sorry, no matching vehicles details could be found.</p>'
     }
     return grid
 }
 
+/* **************************************
+ * Build Error view HTML
+ * ************************************ */
+Util.buildErrorMessage = (heading, quote) => `<section id="error-page">
+    <image src="/images/site/pexels-pixabay-78793.jpg" alt="404 Error Image - broken car" />
+    <div class="error-message">
+    <h2>${heading}</h2>
+    <p class="notice">${quote}</p>
+    </div>
+    </section>`
+
+/* ************************
+ * Constructs the Classification HTML select dropdown
+ ************************** */
+Util.buildClassificationDropdown = async function (req, res, next) {
+    let data = await invModel.getClassifications()
+    // console.log(data)
+
+    // Initialize the list with the opening <select> tag
+    let option = `<select id="classification_id" name="classification_id" value="<%= locals.classification_id %>" required >
+    <option value="" disabled selected>Select a classification</option>`
+
+    // Loop through the rows and add each classification as an <option>
+    data.rows.forEach((row) => {
+        option += `<option value="${row.classification_id}">${row.classification_name}</option>`
+    })
+
+    option += `</select>`
+
+    return option
+}
+
 /* ****************************************
  * Middleware For Handling Errors
- * Wrap other function in this for 
+ * Wrap other function in this for
  * General Error Handling
  **************************************** */
-Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
+Util.handleErrors = (fn) => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
 
 module.exports = Util
